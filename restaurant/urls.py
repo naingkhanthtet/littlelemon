0ef49ec6943e13566031ed1d75d5c293e7ae82d6
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-09-19 14:57:16 +0700

    serializers and views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import MenuItemsView, SingleMenuItemView
+
 
 app_name = "restaurant"
 urlpatterns = [
-    path("", index, name="index"),
+    # path("", index, name="index"),
+    path("menu/", MenuItemsView.as_view()),
+    path("menu/<int:pk>", SingleMenuItemView.as_view()),
 ]
