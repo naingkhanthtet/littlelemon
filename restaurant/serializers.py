fbde06a0ec6960125fd5d87c4da8d2c928d3d550
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-09-20 10:38:30 +0700

    tests

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,5 +1,6 @@
 from .models import Menu, Booking
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, RelatedField
+from django.contrib.auth.models import User, Group
 
 
 class MenuSerializer(ModelSerializer):
@@ -12,3 +13,16 @@ class BookingSerializer(ModelSerializer):
     class Meta:
         model = Booking
         fields = "__all__"
+
+
+class GroupNameField(RelatedField):
+    def to_representation(self, value):
+        return value.name
+
+
+class UserSerializer(ModelSerializer):
+    groups = GroupNameField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("url", "username", "email", "groups")
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,13 @@ from restaurant.models import Menu
 
 
 class MenuTest(TestCase):
-    def test_example(self):
-        # item = Menu.objects.create(title="IceCream", price=80, inventory=10)
-        # self.assertEqual(item, "IceCream : 80")
-        self.assertEqual(1 + 1, 2)
+    def test_create(self):
+        item = Menu.objects.create(title="IceCream", price=80, inventory=10)
+        self.assertEqual(item.title, "IceCream")
+        self.assertEqual(item.price, 80)
+        self.assertEqual(item.inventory, 10)
+
+    def test_delete(self):
+        item = Menu.objects.get(id=self.item.id)
+        item.delete()
+        self.assertEqual(Menu.objects.count(), 0)
